bfccb98296cf3de70c8434380f0c1e744e839506
Svetlin Nakov <nakov@users.noreply.github.com>
2017-03-13 01:50:17 +0200

    Added video YouTube player JS script

---
+++ assets/js/video.js
@@ -0,0 +1,12 @@
+(function(){
+  var styles = document.createElement('style');
+  styles.innerText = '.video-player { width: 100%; position: relative; padding: 2px 0 56.25% 0; height: 0; } .video-player iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }';
+  document.body.prepend(styles);
+  var videoText = document.querySelector('.video-player').textContent;
+  var regex = /embed\/([a-zA-Z0-9_-]+)?/g;
+  var matches = regex.exec(videoText) || [''];
+  var videoId = matches[1];
+  var iframe = "<iframe src='http://www.youtube.com/embed/" + 
+    videoId + "' frameborder='0' allowfullscreen>";
+  document.querySelector('.video-player').innerHTML = iframe;
+})();
