297bb185102855d178ec54b7d55342bb3bc66a6c
Svetlin Nakov <nakov@users.noreply.github.com>
2017-03-13 02:45:57 +0200

    changed the video URL format

--- assets/js/video.js
+++ assets/js/video.js
@@ -3,7 +3,7 @@
   styles.innerText = '.video-player { width: 100%; position: relative; padding: 2px 0 56.25% 0; height: 0; } .video-player iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }';
   document.body.prepend(styles);
   var videoText = document.querySelector('.video-player').textContent;
-  var regex = /embed\/([a-zA-Z0-9_-]+)?/g;
+  var regex = /watch?v=([a-zA-Z0-9_-]+)?/g;
   var matches = regex.exec(videoText) || [''];
   var videoId = matches[1];
   var iframe = "<iframe src='https://www.youtube.com/embed/" + 
