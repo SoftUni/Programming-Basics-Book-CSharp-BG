f0f984c977f20a81a0ae7aeddce8f4f7a6ca729a
Svetlin Nakov <nakov@users.noreply.github.com>
2019-01-08 12:54:03 +0200

    Changed video player script

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,12 +1,12 @@
-(function(){
-  var styles = document.createElement('style');
-  styles.innerText = '.video-player { width: 100%; position: relative; padding: 2px 0 56.25% 0; height: 0; } .video-player iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }';
-  document.body.prepend(styles);
-  var videoText = document.querySelector('.video-player').textContent;
-  var regex = /watch\?v=([a-zA-Z0-9_-]+)?/g;
-  var matches = regex.exec(videoText) || [''];
-  var videoId = matches[1];
-  var iframe = "<iframe src='https://www.youtube.com/embed/" + 
-    videoId + "' frameborder='0' allowfullscreen>";
-  document.querySelector('.video-player').innerHTML = iframe;
-})();
+gitbook.events.bind("page.change", function() {
+    let videos = $(".video-player");
+    for (let video of videos) {
+        let videoText = video.innerText;
+        let regex = /watch\?v=([a-zA-Z0-9_-]+)?/g;
+        let matches = regex.exec(videoText) || [''];
+        let videoId = matches[1];
+        let iframe = "<iframe src='https://www.youtube.com/embed/" + 
+            videoId + "' frameborder='0' allowfullscreen>";
+        $(video).html(iframe);
+    }
+});
